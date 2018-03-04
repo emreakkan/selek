0dbe4f3b6342a6c4620e6ea19fb3de9b225d8151
Emre AKKAN <eakkan4@gmail.com>
2018-03-04 15:57:06 +0300

    Add files via upload

--- selek.js
+++ selek.js
@@ -1,139 +1,138 @@
+/*
+* @author Emre AKKAN
+* @version V1.0
+* @description
+* Select tag replacement
+*/
+
 $.fn.selek = function(params){
-	var selek = this;
-	var selekOpt = selek.children();
-	var option = $.extend({
-		class: '',
-		searchEnable: false,
-		addSearched: false
-	}, params);
+	return $(this).each(function(){
+		var elem = $(this);
+		var elementOpt = $(this).children();
 
-	var selekContainer = document.createElement('div');
-	selekContainer.className = 'selek-wrapper';
+		var option = $.extend({
+			class       : '',
+			isSearch    : false,
+			searchPlace : 'Ara...',
+			addNewItem  : false
+		}, params);
 
-	if(option.class != ''){
-		selekContainer.classList.add(option.class);
-	}
+		var classList = {
+			mainClass     : 'selek-wrapper' + ((option.class !== '') ? ' ' + option.class : ''),
+			titleClass    : 'selek-title',
+			listClass     : 'selek-list',
+			selectedClass : 'selected',
+			openClass     : 'opened',
+			searchClass   : 'selek-search'
+		};
 
-	var selekName = document.createElement('span');
-	selekName.className = 'selek-title';
-	selekName.innerText = selek.find('option:first-child').text();
-	selekName.addEventListener('click', open);
-	selekName.addEventListener('click', open);
+		/*Create select container*/
+		var selekWrapper = document.createElement('div');
+		selekWrapper.className = classList.mainClass;
 
-	var selekList = document.createElement('div');
-	selekList.className = 'selek-list';
-	selekList.addEventListener('click', onChange);
+		/*Create select title element and add event on click*/
+		var selekName = document.createElement('span');
+		selekName.className = classList.titleClass;
+		selekName.addEventListener('click', toggle);
 
-	var ul = document.createElement('ul');
+		/*Create select options list container*/
+		var selekList = document.createElement('div');
+		selekList.className = classList.listClass;
+		selekList.addEventListener('click', change);
 
-	for(var i = 0; i < selekOpt.length; i++){
-		var li = document.createElement('li');
-		li.innerText = $(selekOpt[i]).text();
-		li.setAttribute('data-value', $(selekOpt[i]).val());
-		li.setAttribute('data-index', i);
+		/*Create options list*/
+		var selekUL = document.createElement('ul');
+		for(var i = 0; i < elementOpt.length; i++){
+			var li = document.createElement('li');
+			li.innerText = $(elementOpt[i]).text();
+			li.setAttribute('data-value', $(elementOpt[i]).val());
+			li.setAttribute('data-index', i);
 
-		if(selekOpt[i].getAttribute('selected') !== null){
-			li.classList.add('selected');
-			selekName.innerText = $(selekOpt[i]).text();
-		}
+			if($(elementOpt[i]).is(':selected') !== false){
+				li.classList.add('selected');
+				selekName.innerText = $(elementOpt[i]).text();
+			}
 
-		ul.appendChild(li);
-	}
+			selekUL.appendChild(li);
+		};
 
-	if(option.searchEnable === true){
-		var searchInput = document.createElement('input');
-		searchInput.setAttribute('type', 'search');
-		searchInput.addEventListener('keyup', function(e){
-			searchOpt(e);
-		});
+		/*Search Enable*/
+		if(option.isSearch === true){
+			var searchInput = document.createElement('input');
+			searchInput.setAttribute('type', 'search');
+			searchInput.setAttribute('placeholder', option.searchPlace);
+			searchInput.addEventListener('keyup', function(e){
+				searchOpt(e);
+			});
 
-		var searchContainer = document.createElement('div');
-		searchContainer.className = 'selek-search-wrapper';
-		searchContainer.appendChild(searchInput);
+			var searchWrapper = document.createElement('div');
+			searchWrapper.className = classList.searchClass;
+			searchWrapper.appendChild(searchInput);
 
-		function searchOpt(e){
-			var searchString = $(searchInput).val(),
-				count = 0;
+			function searchOpt(e){
+				var searchString = $(searchInput).val(),
+					count = 0;
 
-			for (var i = 0; i < selekOpt.length; i++) {
-				if(ul.querySelectorAll('li')[i].innerText.search(new RegExp(searchString, "i")) < 0){
-					$(ul.querySelectorAll('li')[i]).hide();
-				} else {
-					count++;
-					$(ul.querySelectorAll('li')[i]).show();
+				for (var i = 0; i < elementOpt.length; i++) {
+					if(selekUL.querySelectorAll('li')[i].innerText.search(new RegExp(searchString, "i")) < 0){
+						$(selekUL.querySelectorAll('li')[i]).hide();
+					} else {
+						count++;
+						$(selekUL.querySelectorAll('li')[i]).show();
+					}
+				}
+
+				if(count == 0 && searchString != '' && e.keyCode === 13){
+					elem.append('<option value="' + searchString + '">' + searchString + '</option>');
+					elementOpt = elem.children();
+
+					var newLi = document.createElement('li');
+					newLi.innerText = searchString;
+					newLi.setAttribute('data-value', searchString);
+					newLi.setAttribute('data-index', elementOpt.length);
+					selekUL.appendChild(newLi);
 				}
 			}
-
-			if(count == 0 && searchString != '' && e.keyCode === 13){
-				selek.append('<option value="' + searchString + '">' + searchString + '</option>');
-				selekOpt = selek.children();
-
-				var newLi = document.createElement('li');
-				newLi.innerText = searchString;
-				newLi.setAttribute('data-value', searchString);
-				newLi.setAttribute('data-index', selekOpt.length);
-				ul.appendChild(newLi);
-			}
-		}
-	}
-
-	selekContainer.appendChild(selekName);
-	selekContainer.appendChild(selekList);
-	if(option.searchEnable === true) selekList.appendChild(searchContainer);
-	selekList.appendChild(ul);
-
-	selek.before(selekContainer, selek);
-	selek.hide();
-
-	document.addEventListener('click', function(e) {
-		if(!selekContainer.contains(e.target)) close();
-	});
-
-	function onChange(e){
-		e.preventDefault();
-		var elem = e.target;
-
-		if(elem.className === selekName){
-			toggle();
 		}
 
-		if(elem.tagName == 'LI'){
-			selekContainer.querySelector('.selek-title').innerText = elem.innerText;
-			$(selek).selectedIndex = elem.getAttribute('data-index');
-			selek.val($(elem).data('value')).change();
+		selekWrapper.appendChild(selekName);
+		selekWrapper.appendChild(selekList);
+		if(option.isSearch === true) selekList.appendChild(searchWrapper);
+		selekList.appendChild(selekUL);
 
-			for (var i = 0; i < selekOpt.length; i++) {
-				ul.querySelectorAll('li')[i].classList.remove('is-selected');
+		elem.before(selekWrapper, elem);
+		elem.hide();
+
+		function change(e){
+			var _this = e.target;
+
+			if(_this.className === selekName){
+				toggle();
 			}
 
-			elem.classList.add('is-selected');
-			close();
+			if(_this.tagName == 'LI'){
+				selekWrapper.querySelector('.' + classList.titleClass).innerText = $(_this).text();
+				$(elem).selectedIndex = _this.getAttribute('data-index');
+				elem.val($(_this).data('value')).change();
+
+				for (var i = 0; i < elementOpt.length; i++) {
+					selekUL.querySelectorAll('li')[i].classList.remove(classList.selectedClass);
+				}
+
+				_this.classList.add(classList.selectedClass);
+				toggle();
+			}
 		}
-	}
 
-
-	function toggle(){
-		selekContainer.classList.toggle('is-opened');
-	}
-
-	function open(){
-		selekContainer.classList.add('is-opened');
-	}
-
-	function close(){
-		selekContainer.classList.remove('is-opened');
-	}
-
-	return{
-		toggle: toggle,
-		close: close,
-		open: open
-	};
-};
-
-$(function(){
-	$('.selek').selek({
-		searchEnable: true,
-		addSearched: true
+		function toggle(){
+			$('.selek-wrapper').not($(selekWrapper)).removeClass(classList.openClass);
+			selekWrapper.classList.toggle(classList.openClass);
+		}
 	});
+}
+
+$(document).click(function(e){
+	if(e.type == 'click' && $(e.target).parents('.selek-wrapper').length < 1){
+		$('.selek-wrapper').removeClass('opened');
+	}
 });
